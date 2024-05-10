f098e5a1f396e04e65d1cf9e8ca63de4df15679b
João Victor <neguchos96@gmail.com>
2024-05-10 10:17:14 -0300

    envioaoservidor

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,5 +3,5 @@ const Contato = require("../models/ContatoModel");
 exports.index = async (req, res) => {
   const contatos = await Contato.buscaContatos();
   res.json("Olá mundo porra");
-  res.render({ contatos });
+  console.log("Olá mundo");
 };
