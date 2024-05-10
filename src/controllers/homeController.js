6e787b9296141fcb20284c5a1e31920408049205
João Victor <neguchos96@gmail.com>
2024-05-10 09:35:49 -0300

    commit inicial

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,6 @@ const Contato = require("../models/ContatoModel");
 
 exports.index = async (req, res) => {
   const contatos = await Contato.buscaContatos();
-  res.json("Olá mundo");
+  res.json("Olá mundo porra");
   // res.render("index", {contatos})
 };
